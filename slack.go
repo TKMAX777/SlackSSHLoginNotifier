b6418469765014ca707a2d1d50a725d8747f5ff6
TKMAX777 <greatbananachips-service@yahoo.co.jp>
2021-08-13 13:49:50 +0900

    add client ip

--- slack.go
+++ slack.go
@@ -12,7 +12,7 @@ func SendLoginMessage() error {
 	var message = slack.Webhook{
 		IconEmoji: ":key:",
 		UserName:  fmt.Sprintf("[%s] SSH Login Alert", strings.ToUpper(HostName)),
-		Text:      fmt.Sprintf("%s has logined at %s", UserName, time.Now().Format(time.UnixDate)),
+		Text:      fmt.Sprintf("%s has logined at %s\n from: %s", UserName, time.Now().Format(time.UnixDate), ClientIP),
 	}
 
 	return Slack.Send(message)
